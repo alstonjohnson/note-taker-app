b4780ed1ec6f84d2ce3e0e018a5a0ace35fb7f8a
alstonjohnson <johnson.alston@icloud.com>
2024-08-26 14:08:16 -0500

    app.listener

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Please check localhost${PORT}`);
+  });
