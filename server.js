1f0f4ff37edab0476a35e0e24aea266d8649987b
alstonjohnson <johnson.alston@icloud.com>
2024-08-26 14:31:13 -0500

    added wildcard to server.js

--- server.js
+++ server.js
@@ -12,15 +12,21 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// GET Route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
   );
   
-  // GET Route for feedback page
   app.get('/feedback', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
   );
+
+
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/404.html'))
+  );
+
+
 
 app.listen(PORT, () => {
     console.log(`Please check localhost${PORT}`);
