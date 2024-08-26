bb70df4eb63ac446a8529fd3dd8921faed73b4f9
alstonjohnson <johnson.alston@icloud.com>
2024-08-26 14:09:14 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
 app.listen(PORT, () => {
     console.log(`Please check localhost${PORT}`);
   });
