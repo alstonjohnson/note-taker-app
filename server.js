a73033304c16d72cd19abaf7a3ca2d7fe4fa9bcd
alstonjohnson <johnson.alston@icloud.com>
2024-08-27 10:27:36 -0500

    started work on apiRoutes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
   );
