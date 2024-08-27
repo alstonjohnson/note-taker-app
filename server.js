63a0687c6cfea153da24b33e67d0ee811e9bd329
alstonjohnson <johnson.alston@icloud.com>
2024-08-27 15:54:08 -0500

    finally figured it out

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
-
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const db = require("./db/db")
-const fs = require('fs')
+const fs = require('fs');
 
+let db = [];
 
 function readNotesFromFile(callback) {
     fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
@@ -14,7 +12,10 @@ function readNotesFromFile(callback) {
             console.error(err);
             return;
         }
-        database = JSON.parse(data);
+        db = JSON.parse(data);
+        if (callback) {
+            callback(db);
+        }
     });
 }
 
@@ -22,9 +23,6 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -34,60 +32,38 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/404.html'))
+app.get('/api/notes', (req, res) =>
+    res.json(db)
 );
 
+app.post('/api/notes', (req, res) => {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    let newNote = req.body;
 
-app.route("/api/notes")
-    .get(function (req, res) {
-        readNotesFromFile((data) => {
-            res.json(data);
-        });
-    })
-
-
-    .post(function (req, res) {
-        let jsonFilePath = path.join(__dirname, "/db/db.json");
-        let newNote = req.body;
-
-        // This allows the test note to be the original note.
-        let highestId = 0;
-        // This loops through the array and finds the highest ID.
-        for (let i = 0; i < db.length; i++) {
-            let individualNote = db[i];
-
-            if (individualNote.id > highestId) {
-                // highestId will always be the highest numbered id in the notesArray.
-                highestId = individualNote.id;
-            }
+    // Find the highest ID
+    let highestId = 0;
+    for (const note of db) {
+        if (note.id > highestId) {
+            highestId = note.id;
         }
-        // This assigns an ID to the newNote. 
-        newNote.id = highestId + 1;
-        // We push it to db.json.
-        db.push(newNote)
+    }
+    // This assigns an ID to the newNote. 
+    newNote.id = highestId + 1;
+    // We push it to db.json.
+    db.push(newNote)
 
-        // Write the db.json file again.
-        fs.writeFile(jsonFilePath, JSON.stringify(db), function (err) {
-
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Your note was saved!");
-        });
-        // Gives back the response, which is the user's new note. 
+    fs.writeFile(jsonFilePath, JSON.stringify(db), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Your note was saved!');
         res.json(newNote);
     });
-
-
+});
 
 readNotesFromFile();
 
-
-
-
 app.listen(PORT, () => {
-    console.log(`Please check http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
